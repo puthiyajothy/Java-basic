5e3865f14c9ff418093a73d6c040a0fcdcd9c965
puthiyajothi <saidputhi@gmail.com>
2019-08-10 19:49:19 +0530

    sample

---
+++ src/com/java/basic/CallectionList.java
@@ -0,0 +1,20 @@
+package com.java.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallectionList {
+
+	public static void main(String[] args) {
+		
+		List<Integer>cal = new ArrayList();
+		cal.add(10);
+		cal.add(5);
+		cal.add(6);
+		
+		System.out.println(cal);
+		
+
+	}
+
+}
